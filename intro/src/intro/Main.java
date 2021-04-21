99fdf82aabb5ba889318612147587bb226220b98
Abrek06 <ozkandemirel06@gmail.com>
2021-04-21 23:49:18 +0300

    Deneme

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -28,6 +28,19 @@ public class Main {
 			System.out.println(kredi[i]);
 		}
 		
+		int sayi1=10;
+		int sayi2=20;
+		sayi1=sayi2;
+		sayi2=100;
+		System.out.println(sayi1);//20
+		
+		int[] sayilar1 = {1,2,3,4,5};
+		int[] sayilar2 = {10,20,30,40,50};
+		sayilar1 = sayilar2;
+		sayilar2[0]=100;
+		System.out.println(sayilar1[0]);//100
+		
+		
 
 	}
 
